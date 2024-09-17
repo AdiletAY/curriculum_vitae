ff148c50a58541c8d91a15f0746d38628c293701
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2024-09-17 14:53:12 +0500

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./global.css"
+import HomePage from "@/pages/home/HomePage.tsx";
+
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div>
+            This is a Home Page!
+        </div>
+    );
+};
+
+export default HomePage;
