3d725f24fda8cc79db265885d7599d5ad9ecc724
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2024-09-26 01:55:01 +0500

    add: Header and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import "./global.css"
 import HomePage from "@/pages/home/HomePage.tsx";
 
@@ -6,7 +8,9 @@ function App() {
 
   return (
     <>
+      <Header/>
       <HomePage />
+      <Footer />
     </>
   )
 }
