a3672e27a7d6a9ebe1f6dc1f00008811cc4cfdf6
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2024-09-26 01:53:25 +0500

    add: Objective Card

---
+++ src/components/ObjectiveCard.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+interface ObjectiveCardProps {
+  title: string;
+  introduction: string;
+  description: string;
+}
+
+const ObjectiveCard: FC<ObjectiveCardProps> = ({ title, introduction, description }) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <span className="text-2xl font-black text-gray-900 dark:text-white">
+            {title}
+          </span>
+        </CardTitle>
+      </CardHeader>
+      
+      <CardContent>
+        <p className="text-lg font-semibold text-gray-900 dark:text-white">
+          {introduction}
+        </p>
+        <p className="my-3 text-gray-500 dark:text-gray-400">
+          {description}
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ObjectiveCard;
