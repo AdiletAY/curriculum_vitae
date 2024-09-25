10fe649078ae3160749591d5cb138009d81a13ad
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2024-09-26 01:53:41 +0500

    add: Personal Info Card

---
+++ src/components/PersonalInfoCard.tsx
@@ -0,0 +1,20 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const PersonalInfoCard = () => {
+  return (
+    <Card>
+        <CardHeader>
+        <CardTitle>
+                <span className="text-2xl font-black text-gray-900 dark:text-white">
+                    Personal Information
+                </span>
+            </CardTitle>
+        </CardHeader>
+        <CardContent>
+                
+        </CardContent>  
+    </Card>
+  )
+}
+
+export default PersonalInfoCard
