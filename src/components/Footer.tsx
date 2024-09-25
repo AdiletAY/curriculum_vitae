96cc95bc0cd3bce7c8cbf26af3436aace27274d7
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2024-09-26 01:52:46 +0500

    add: footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-white dark:bg-gray-900">
+        <div className="w-full mx-auto max-w-screen-xl p-4">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                © 2024. All Rights Reserved.    
+            </span>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
