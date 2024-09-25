c8b5f539a4f9e09f735194cda2b7b06ab748b50c
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2024-09-26 01:54:49 +0500

    update: Home Page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,32 @@
+import ProfileCard from "@/components/ProfileCard";
+import ObjectiveCard from "@/components/ObjectiveCard";
+import { objectiveData } from "@/data/objective";
+import WorkExperienceCard from "@/components/WorkExperienceCard";
+import SkillCard from "@/components/SkillCard";
+import { skills } from "@/data/skills";
+
 const HomePage = () => {
     return (
-        <div>
-            This is a Home Page!
+        <div className="container">
+            <div className='flex justify-between items-start gap-5'>
+                <ProfileCard 
+                        name="Adilet Alpysbay" 
+                        jobTitle="Python Backend Developer" 
+                        avatarUrl="https://github.com/shadcn.png" 
+                        buttonLabel="Download CV" 
+                />
+
+                <div className='w-full flex flex-col gap-5'>
+                    <ObjectiveCard 
+                        title={objectiveData?.title} 
+                        introduction={objectiveData?.introduction} 
+                        description={objectiveData?.description}
+                    />
+                    
+                    <WorkExperienceCard/>
+                    <SkillCard skills={skills}/>
+                </div>   
+            </div>
         </div>
     );
 };
