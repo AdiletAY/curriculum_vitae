2cac99003b70460bf8a5e603e9abe8c964511e34
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2024-09-26 02:07:07 +0500

    update: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/curriculum_vitae/"
+  base: "/curriculum_vitae/",
+  build:{
+    outDir: "build"
+  }
 })
