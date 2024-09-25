9674430b73cadf3301ff8d71673fe5978d212048
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2024-09-26 01:54:06 +0500

    add: Skill Card

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+interface SkillCardProps {
+  skills: Array<string>
+}
+
+const SkillCard: FC<SkillCardProps> = ({ skills }) => {
+  return (
+
+    <Card>
+        <CardHeader>
+            <CardTitle>
+                <span className="text-2xl font-black text-gray-900 dark:text-white">
+                    Skills
+                </span>
+            </CardTitle>
+        </CardHeader>
+        <CardContent>
+            {skills.map((skill, index) => (
+                <span key={index} className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-3 py-1 rounded dark:bg-blue-900 dark:text-blue-300">
+                    {skill}
+                </span>
+            ))}
+        </CardContent>
+    </Card>
+
+  );
+}
+
+export default SkillCard;
